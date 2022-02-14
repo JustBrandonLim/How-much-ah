b454b4604fce9145c29a46ab2cfc562021f6d3af
Brandon <brandonlimjingzhi@gmail.com>
2022-02-15 06:34:04 +0800

    Added exception handling to middleware.

--- functions/bot/actions/billMiddleware.js
+++ functions/bot/actions/billMiddleware.js
@@ -1,25 +1,29 @@
 module.exports = async (ctx, next) => {
-  if (ctx.updateType === "message") {
-    const text = ctx.update.message.text.toLowerCase();
-    if (text.startsWith("/")) {
-      const match = text.match(/^\/([^\s]+)\s?(.+)?/);
-      let args = [];
-      let command;
-      if (match !== null) {
-        if (match[1]) {
-          command = match[1];
+  try {
+    if (ctx.updateType === "message") {
+      const text = ctx.update.message.text.toLowerCase();
+      if (text.startsWith("/")) {
+        const match = text.match(/^\/([^\s]+)\s?(.+)?/);
+        let args = [];
+        let command;
+        if (match !== null) {
+          if (match[1]) {
+            command = match[1];
+          }
+          if (match[2]) {
+            args = match[2].split(" ");
+          }
         }
-        if (match[2]) {
-          args = match[2].split(" ");
-        }
-      }
 
-      ctx.state = {
-        raw: text,
-        command,
-        args,
-      };
+        ctx.state = {
+          raw: text,
+          command,
+          args,
+        };
+      }
     }
+  } catch (error) {
+    console.log(error);
   }
 
   return next();
--- functions/bot/actions/start.js
+++ functions/bot/actions/start.js
@@ -1,4 +1,4 @@
 module.exports = async (ctx) => {
   console.log(ctx.chat.username + " used /start!");
-  return ctx.reply("Hello there! Nice to meet you! I am How much ah? 🥰");
+  return ctx.reply("Hello there! Nice to meet you! I am How much ah? You can use me to help split your bills with your friends!");
 };
