5a160d2d71443898f83c39a45640a657559d9a37
Brandon <brandonlimjingzhi@gmail.com>
2022-02-15 05:31:24 +0800

    Testing code.

--- functions/bot/actions/newBill.js
+++ functions/bot/actions/newBill.js
@@ -1,7 +1,9 @@
 module.exports = async (ctx) => {
   console.log(ctx.chat.username + " used " + ctx.state.raw);
   try {
-    const perPersonBill = ctx.state.args[0] / ctx.state.args[1];
+    const totalAmount = ctx.state.args[0].replace("$", "");
+    const totalPax = ctx.state.args[1];
+    const perPersonBill = parseInt(totalAmount) / parseInt(totalPax);
     return ctx.reply("Each person would have to pay $" + perPersonBill.toFixed(2));
   } catch (error) {
     return ctx.reply("Oops! Something went wrong!");
